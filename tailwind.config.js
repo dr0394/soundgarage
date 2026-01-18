82fbac71392e82d4ec30a329c0ead4da59ee22f3
webflixinfo <kontakt@webflix.info>
2026-01-18 23:14:57 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          50: '#E8EEF7',
+          100: '#D1DDEF',
+          200: '#A3BBDF',
+          300: '#7599CF',
+          400: '#5481C0',
+          500: '#3D68B1',
+          600: '#3D68B1',
+          700: '#2F5189',
+          800: '#223A61',
+          900: '#162339',
+        },
+      },
+    },
   },
   plugins: [],
 };
